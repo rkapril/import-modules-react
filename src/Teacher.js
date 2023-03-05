4fef45def1cab0cc18202e9b85433b12ce79a5f7
rkapril <rkapril@me.com>
2023-03-06 00:38:35 +0800

    updated index & teacher js files

--- src/Teacher.js
+++ src/Teacher.js
@@ -1,6 +1,10 @@
 import { CoolPerson } from "./Cool Person";
 
-export class Teacher extends CoolPerson {
+export function promote() {
+  console.log("promote")
+}
+
+export default class Teacher extends CoolPerson {
   constructor(name, degree) {
     super(name);
     this.degree = degree;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import { Teacher } from "./Teacher";
+import Teacher from "./Teacher"; // Default Export
+import { promote } from "./Teacher"; // Named Export
+// import Teacher, { promote } from "./Teacher";
+// import React, { Component } from 'react';
 
 const teacher = new Teacher("Mosh", "MSc");
 teacher.teach();
+promote();
